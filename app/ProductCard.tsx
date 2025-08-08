e65af90acc7c38fb6504d42ec809d057b2a5fc43
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-08-08 19:47:45 +0530

    connecting to db fetch and displaying products

--- app/ProductCard.tsx
+++ app/ProductCard.tsx
@@ -1,18 +1,20 @@
-import { Product } from '@/lib/mocks';
 import { formatPrice } from '@/lib/utils';
 import Image from 'next/image';
+import { Product } from './generated/prisma';
 
 function ProductCard({ product }: { product: Product }) {
 	return (
 		<div className='border p-4 rounded-lg shadow hover:shadow-lg transition-shadow'>
 			<div className='relative w-full h-48 mb-4'>
-				<Image
-					src={product.imageUrl}
-					alt={product.name}
-					sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-					className='object-cover'
-					fill
-				/>
+				{product.imageUrl && (
+					<Image
+						src={product.imageUrl}
+						alt={product.name}
+						sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+						className='object-cover'
+						fill
+					/>
+				)}
 			</div>
 			<h2 className='text-xl font-semibold mb-2'>{product.name}</h2>
 			<p className='text-gray-700 mb-2'>{formatPrice(product.price)}</p>
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
-import { mockProducts } from '@/lib/mocks';
 import ProductCard from './ProductCard';
+import prisma from '@/lib/prisma';
 
-export default function HomePage() {
+export default async function HomePage() {
+	const products = await prisma.product.findMany();
 	return (
 		<main className='container mx-auto p-4'>
 			<h1 className='text-3xl font-bold mb-6'>Home Page</h1>
-			<p className='mb-5'>Showing {mockProducts.length} products</p>
+			<p className='mb-5'>Showing {products.length} products</p>
 			<div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
-				{mockProducts.map((product) => (
+				{products.map((product) => (
 					<ProductCard key={product.id} product={product} />
 				))}
 			</div>

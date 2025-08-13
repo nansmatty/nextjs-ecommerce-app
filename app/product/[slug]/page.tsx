decdabdfeef2ce5f096497e4c0464199c811eb1e
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-08-14 00:03:03 +0530

    imporving the single product page UI

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,8 +1,10 @@
 import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { Separator } from '@/components/ui/separator';
 import { getProductBySlug } from '@/lib/actions';
 import { formatPrice } from '@/lib/utils';
+import { HeartIcon, ShoppingCart } from 'lucide-react';
 import Image from 'next/image';
 import { notFound } from 'next/navigation';
 
@@ -59,6 +61,35 @@ const ProductPage = async ({ params }: { params: Promise<{ slug: string }> }) =>
 						<div className='space-y-2'>
 							<h2 className='font-medium'>Description</h2>
 							<p>{product.description}</p>
+						</div>
+						<Separator className='my-4' />
+						<div className='space-y-2'>
+							<h2 className='font-medium'>Availablity</h2>
+							<div className='flex items-center gap-2'>
+								{product.inventory > 0 ? (
+									<Badge variant='outline' className='text-green-600 font-semibold'>
+										In Stock
+									</Badge>
+								) : (
+									<Badge variant='outline' className='text-red-600 font-semibold'>
+										Out of Stock
+									</Badge>
+								)}
+
+								{product.inventory > 0 && <span className='text-xs text-gray-500'>({product.inventory} items available)</span>}
+							</div>
+							<Separator className='my-4' />
+
+							<div className='flex items-center gap-4'>
+								<Button disabled={product.inventory === 0} className='px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors'>
+									<ShoppingCart className='w-4 h-4' />
+									{product.inventory > 0 ? 'Add to Cart' : 'Out of Stock'}
+								</Button>
+								<Button className='px-4 py-2 bg-gray-200 text-gray-800 rounded hover:bg-gray-300 transition-colors'>
+									<HeartIcon className='w-4 h-4' />
+									Add to Wishlist
+								</Button>
+							</div>
 						</div>
 					</div>
 				</CardContent>

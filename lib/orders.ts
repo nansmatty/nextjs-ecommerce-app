4b6c37cd211577d67636f56d14c5314de6e111f9
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-08-26 18:47:44 +0530

    improving the process checkout function

--- lib/orders.ts
+++ lib/orders.ts
@@ -3,14 +3,17 @@
 import { cookies } from 'next/headers';
 import { getCart } from './actions';
 import prisma from './prisma';
+import { createCheckoutSession } from './stripe';
 
-export async function createOrder() {
+export async function processCheckout() {
 	const cart = await getCart();
 
 	if (!cart || cart.items.length === 0) {
 		throw new Error('Cart is empty');
 		1;
 	}
+
+	let orderId: string | null = null;
 
 	try {
 		const order = await prisma.$transaction(async (tx) => {
@@ -48,19 +51,58 @@ export async function createOrder() {
 			return newOrder;
 		});
 
+		orderId = order.id;
+
+		// 1. Reload full order
+
+		const fullOrder = await prisma.order.findUnique({
+			where: { id: order.id },
+			include: {
+				items: {
+					include: {
+						product: true,
+					},
+				},
+			},
+		});
+
+		// 2. Confirm the order was loaded
+
+		if (!fullOrder) {
+			throw new Error('Order not found.');
+		}
+
+		// 3. Create the stripe session
+
+		const { sessionId, sessionUrl } = await createCheckoutSession(fullOrder);
+
+		// 4. Return the session url and handle the errors
+
+		if (!sessionId || !sessionUrl) {
+			throw new Error('Failed to create checkout session.');
+		}
+
+		// 5. Store the session id in the order & change the order status
+
+		await prisma.order.update({
+			where: { id: order.id },
+			data: { stripeSessionId: sessionId, status: 'pending' },
+		});
+
 		(await cookies()).delete('cartId');
 
 		return order;
 	} catch (error) {
+		// 1. OPTIONAL: the change the order status to failed
+
+		if (orderId && error instanceof Error && error.message.includes('Stripe')) {
+			await prisma.order.update({
+				where: { id: orderId },
+				data: { status: 'failed' },
+			});
+		}
+
 		console.error('Error creating order:', error);
 		throw new Error('Failed to create order');
 	}
-
-	// TODO:
-	// 1. Calculate total price
-	// 2. Create order record
-	// 3. Create order items records
-	// 4. Clear cart
-	// 5. Revalidate cache
-	// 6. Return the order
 }

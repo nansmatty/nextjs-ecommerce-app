7744702e1bc5da2452de5e75a02fcb8656b9d497
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-08-27 23:44:26 +0530

    shadcn label and auth signin and signup basic structure design

---
+++ app/auth/signup/page.tsx
@@ -0,0 +1,41 @@
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import Link from 'next/link';
+
+export default function SignUpPage() {
+	return (
+		<main className='flex min-h-screen flex-col items-center justify-center p-4'>
+			<Card className='w-full max-w-md'>
+				<CardHeader>
+					<CardTitle className='text-center'>Create account</CardTitle>
+					<CardDescription className='text-center'>Enter your email and password to create an account</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<form className='space-y-4'>
+						<div className='space-y-2'>
+							<Label htmlFor='email'>Email</Label>
+							<Input type='email' id='email' placeholder='Email' />
+						</div>
+						<div className='space-y-2'>
+							<Label htmlFor='password'>Password</Label>
+							<Input type='password' id='password' placeholder='Password' />
+						</div>
+						<Button type='submit' className='w-full'>
+							Sign Up
+						</Button>
+					</form>
+				</CardContent>
+				<CardFooter>
+					<div className='w-full flex justify-center text-sm'>
+						<p>Already have an account ?</p>&nbsp;{' '}
+						<Link href='/auth/signin' className='text-primary hover:underline'>
+							Sign In
+						</Link>
+					</div>
+				</CardFooter>
+			</Card>
+		</main>
+	);
+}

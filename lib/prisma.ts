8dd46e3f548d471ab8b661b3261f3ae8727cf517
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-08-31 14:55:39 +0530

    fix: change PrismaClient logging level from 'query' to 'error' for better error tracking

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,7 +4,7 @@ const globalForPrisma = globalThis as unknown as {
 	prisma: PrismaClient | undefined;
 };
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient({ log: ['query'] });
+const prisma = globalForPrisma.prisma ?? new PrismaClient({ log: ['error'] });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
